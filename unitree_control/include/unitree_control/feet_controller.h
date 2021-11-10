afdcab2b55e84a71ea369842d66f81ba04101ac4
qiayuan <liaoqiayuan@gmail.com>
2021-11-10 23:05:44 +0800

    Add FeetCmd.msg and cmd_feet interface

--- unitree_control/include/unitree_control/feet_controller.h
+++ unitree_control/include/unitree_control/feet_controller.h
@@ -5,34 +5,40 @@
 #pragma once
 #include "legs_controller.h"
 #include "foot_swing_trajectory.h"
+#include <unitree_msgs/FeetCmd.h>
 
 namespace unitree_ros
 {
-enum FootState
-{
-  TOUCH,
-  SWING
-};
-
-struct FootData
-{
-  FootState state_;
-  double phase_, swing_time_;
-};
-
 class FeetController : public LegsController
 {
 public:
+  enum TouchState
+  {
+    STAND = 0,
+    SWING
+  };
+  struct State
+  {
+    TouchState touch_state_;
+    ros::Time take_off_time_;
+    double phase_, swing_time_;
+  };
+
   FeetController() = default;
-  void updateData(const ros::Time& time, const ros::Duration& period) override;
+  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& controller_nh) override;
   void updateCommand(const ros::Time& time, const ros::Duration& period) override;
-  FootData& getFootData(LegPrefix leg);
+  State& getFootState(LegPrefix leg);
   void setSwing(LegPrefix leg, const Eigen::Vector3d& final_pos, double swing_time);
   void setTouch(LegPrefix leg, const Eigen::Vector3d& force);
 
 private:
-  FootSwingTrajectory<double> feet_swing_trajectory_[4];
-  FootData feet_data_[4];
+  void feetCmdCallback(const unitree_msgs::FeetCmd::ConstPtr& msg);
+
+  FootSwingTrajectory<double> swing_trajectory_[4];
+  State states_[4];
+
+  ros::Subscriber feet_cmd_sub_;
+  realtime_tools::RealtimeBuffer<unitree_msgs::FeetCmd> feet_cmd_buffer_;
 };
 
 }  // namespace unitree_ros
--- unitree_control/src/feet_controller.cpp
+++ unitree_control/src/feet_controller.cpp
@@ -7,24 +7,45 @@
 
 namespace unitree_ros
 {
-void FeetController::updateData(const ros::Time& time, const ros::Duration& period)
+bool FeetController::init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& controller_nh)
 {
-  LegsController::updateData(time, period);
+  if (!LegsController::init(robot_hw, controller_nh))
+    return false;
+
+  // ROS Topic
+  feet_cmd_sub_ =
+      controller_nh.subscribe<unitree_msgs::FeetCmd>("/cmd_feet", 1, &FeetController::feetCmdCallback, this);
+  return true;
 }
 
 void FeetController::updateCommand(const ros::Time& time, const ros::Duration& period)
 {
+  // Update Command from ROS topic interface.
+  unitree_msgs::FeetCmd cmd_msgs = *feet_cmd_buffer_.readFromRT();
+  for (size_t j = 0; j < cmd_msgs.leg_prefix.size(); ++j)
+  {
+    State& state = states_[cmd_msgs.leg_prefix[j].prefix];
+    if (cmd_msgs.header.stamp > state.take_off_time_)
+    {
+      state.take_off_time_ = cmd_msgs.header.stamp;
+      Eigen::Vector3d pos_final(cmd_msgs.pos_final[j].x, cmd_msgs.pos_final[j].y, cmd_msgs.pos_final[j].z);
+      if (cmd_msgs.touch_state[j] == cmd_msgs.SWING)
+        setSwing(LegPrefix(cmd_msgs.leg_prefix[j].prefix), pos_final, cmd_msgs.swing_time[j]);
+    }
+  }
+
   for (int leg = 0; leg < 4; ++leg)
   {
-    if (feet_data_[leg].state_ == SWING)
+    if (states_[leg].touch_state_ == SWING)
     {
-      feet_data_[leg].phase_ += period.toSec() / feet_data_[leg].swing_time_;
-      if (feet_data_[leg].phase_ > 1.)
-        feet_data_[leg].phase_ = 1.;
-      feet_swing_trajectory_[leg].computeSwingTrajectoryBezier(feet_data_[leg].phase_, feet_data_[leg].swing_time_);
+      states_[leg].phase_ += period.toSec() / states_[leg].swing_time_;
+      if (states_[leg].phase_ > 1.)
+        states_[leg].phase_ = 1.;
+      swing_trajectory_[leg].computeSwingTrajectoryBezier(states_[leg].phase_, states_[leg].swing_time_);
       LegsController::Command leg_cmd;
-      leg_cmd.foot_pos_des_ = feet_swing_trajectory_[leg].getPosition();
-      leg_cmd.foot_vel_des_ = feet_swing_trajectory_[leg].getVelocity();
+      leg_cmd.stamp_ = time;
+      leg_cmd.foot_pos_des_ = swing_trajectory_[leg].getPosition();
+      leg_cmd.foot_vel_des_ = swing_trajectory_[leg].getVelocity();
       leg_cmd.kp_cartesian_ << 700, 0, 0, 0, 700, 0, 0, 0, 150;
       leg_cmd.kd_cartesian_ << 7, 0, 0, 0, 7, 0, 0, 0, 7;
       setLegCmd(LegPrefix(leg), leg_cmd);
@@ -41,23 +62,28 @@ void FeetController::updateCommand(const ros::Time& time, const ros::Duration& p
   LegsController::updateCommand(time, period);
 }
 
-FootData& FeetController::getFootData(LegPrefix leg)
+FeetController::State& FeetController::getFootState(LegPrefix leg)
 {
-  return feet_data_[leg];
+  return states_[leg];
 }
 
 void FeetController::setSwing(LegPrefix leg, const Eigen::Vector3d& final_pos, double swing_time)
 {
-  feet_data_[leg].state_ = SWING;
-  feet_data_[leg].phase_ = 0.;
-  feet_data_[leg].swing_time_ = swing_time;
-  feet_swing_trajectory_[leg].setHeight(0.1);
-  feet_swing_trajectory_[leg].setInitialPosition(getLegState(leg).foot_pos_);
-  feet_swing_trajectory_[leg].setFinalPosition(final_pos);
+  states_[leg].touch_state_ = SWING;
+  states_[leg].phase_ = 0.;
+  states_[leg].swing_time_ = swing_time;
+  swing_trajectory_[leg].setHeight(0.1);
+  swing_trajectory_[leg].setInitialPosition(getLegState(leg).foot_pos_);
+  swing_trajectory_[leg].setFinalPosition(final_pos);
 }
 
 void FeetController::setTouch(LegPrefix leg, const Eigen::Vector3d& force)
 {
+}
+
+void FeetController::feetCmdCallback(const unitree_msgs::FeetCmd::ConstPtr& msg)
+{
+  feet_cmd_buffer_.writeFromNonRT(*msg);
 }
 
 }  // namespace unitree_ros
