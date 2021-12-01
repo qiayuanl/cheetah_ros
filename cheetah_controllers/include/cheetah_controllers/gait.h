77f7902e4219c3dbafbdbb8dca7a2f8fd83ded60
qiayuan <liaoqiayuan@gmail.com>
2021-12-01 15:17:29 +0800

    Add OffsetDurationGaitRos

--- cheetah_controllers/include/cheetah_controllers/gait.h
+++ cheetah_controllers/include/cheetah_controllers/gait.h
@@ -6,6 +6,7 @@
 
 #include <ros/ros.h>
 #include <unitree_control/cpp_types.h>
+#include <unitree_common/ros_utilities.h>
 
 namespace unitree_ros
 {
@@ -13,9 +14,10 @@ template <typename T>
 class OffsetDurationGait
 {
 public:
-  OffsetDurationGait(int segment, T gait_cycle, const Vec4<T>& offsets, const Vec4<T>& durations)
-    : segment_(segment), mpc_dt_(gait_cycle / static_cast<T>(segment_)), offsets_(offsets), durations_(durations)
+  OffsetDurationGait(int segment, T cycle, const Vec4<T>& offsets, const Vec4<T>& durations)
+    : segment_(segment), mpc_dt_(cycle / static_cast<T>(segment_)), offsets_(offsets), durations_(durations)
   {
+    mpc_table_ = new int[segment_ * 4];
   }
 
   ~OffsetDurationGait()
@@ -33,15 +35,15 @@ public:
     for (int i = 0; i < segment_; i++)
     {
       int iter = (i + iteration_ + 1) % segment_;
-      Eigen::Array4i progress = iter - offsets_;
       for (int j = 0; j < 4; j++)
       {
-        if (progress[j] < 0)
-          progress[j] += segment_;
-        if (progress[j] < durations_[j])
-          mpc_table_[i * 4 + j] = 1;
-        else
+        int progress = iter - offsets_[i] * segment_;
+        if (progress < 0)
+          progress += segment_;
+        if (progress > durations_[i] * segment_)
           mpc_table_[i * 4 + j] = 0;
+        else
+          mpc_table_[i * 4 + j] = 1;
       }
     }
     return mpc_table_;
@@ -70,13 +72,55 @@ public:
     return durations_ * mpc_dt_ * segment_;
   }
 
+  Vec4<T> getStandTime()
+  {
+    Vec4<T> ones;
+    ones.setOnes();
+    return (ones - durations_) * mpc_dt_ * segment_;
+  }
+
+  void resizeSegment(int segment)
+  {
+    delete[] mpc_table_;
+    segment_ = segment;
+    mpc_table_ = new int[segment_ * 4];
+  }
+
 private:
   int segment_, iteration_;
   T mpc_dt_;
   int* mpc_table_;
 
-  Eigen::Array4d offsets_;    // offset in mpc segments
-  Eigen::Array4d durations_;  // duration of step in mpc segments
+  Vec4<T> offsets_;    // offset in 0.0 ~ 1.0
+  Vec4<T> durations_;  // duration of step in 0.0 ~ 1.0
+};
+
+template <typename T>
+class OffsetDurationGaitRos : public OffsetDurationGait<T>
+{
+public:
+  OffsetDurationGaitRos<T>(XmlRpc::XmlRpcValue& params, int segment)
+  {
+    ROS_ASSERT(params.hasMember("cycle"));
+    ROS_ASSERT(params.hasMember("offsets"));
+    ROS_ASSERT(params.hasMember("durations"));
+    OffsetDurationGait<T>(segment, xmlRpcGetDouble(params["cycle"]), getVec4(params, "offsets"),
+                          getVec4(params, "durations"));
+  }
+
+private:
+  Vec4<T> getVec4(XmlRpc::XmlRpcValue& params, const std::string& name)
+  {
+    Vec4<T> array;
+    if (params.hasMember(name))
+      if (params.getType() == XmlRpc::XmlRpcValue::TypeArray)
+      {
+        if (params[name].size() == 4)
+          array << xmlRpcGetDouble(params[name], 0), xmlRpcGetDouble(params[name], 1), xmlRpcGetDouble(params[name], 2),
+              xmlRpcGetDouble(params[name], 3);
+      }
+    return array;
+  }
 };
 
 }  // namespace unitree_ros
