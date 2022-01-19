0d7c3bdd5cc3c48fb644d5eac1e762fd29e18382
qiayuan <liaoqiayuan@gmail.com>
2022-01-19 17:37:16 +0800

    Try constant accel trajectory

--- cheetah_mpc_controllers/test/trajectory_test.cpp
+++ cheetah_mpc_controllers/test/trajectory_test.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 
   ros::NodeHandle nh;
   ros::Publisher pub = nh.advertise<trajectory_msgs::JointTrajectory>("/traj", 100);
-  ros::Rate loop_rate(0.5);
+  ros::Rate loop_rate(10);
 
   trajectory_msgs::JointTrajectory traj;
   traj.joint_names.push_back("roll");
@@ -23,25 +23,25 @@ int main(int argc, char** argv)
   traj.joint_names.push_back("z");
   traj.header.stamp = ros::Time(0.5);
 
-  double vel_x = 1.;
-  double vel_z = 3.;
-  double rate_pitch_deg = 100.;
-  double duration = 0.15;
+  double acc_x = 7.5;
+  double acc_z = 15;
+  double acc_pitch_deg = 500.;
+  double duration = 0.2;
   int points = 10;
   double dt = duration / static_cast<double>(points);
   for (int i = 1; i < points + 1; ++i)
   {
-    double rate_pitch = rate_pitch_deg / 180. * M_PI;
+    double acc_pitch = acc_pitch_deg / 180. * M_PI;
     trajectory_msgs::JointTrajectoryPoint point;
     point.positions.resize(6);
     point.velocities.resize(6);
     point.time_from_start = ros::Duration(dt * i);
-    point.velocities[1] = rate_pitch;
-    point.velocities[3] = vel_x;
-    point.velocities[5] = vel_z;
-    point.positions[1] = rate_pitch * i * dt;
-    point.positions[3] = vel_x * i * dt;
-    point.positions[5] = 0.1 + vel_z * i * dt;
+    point.velocities[1] = acc_pitch * i * dt;
+    point.velocities[3] = acc_x * i * dt;
+    point.velocities[5] = acc_z * i * dt;
+    point.positions[1] = 1. / 2. * acc_pitch * (i * dt) * (i * dt);
+    point.positions[3] = 1. / 2. * acc_x * (i * dt) * (i * dt);
+    point.positions[5] = 0.056 + 1. / 2. * acc_z * (i * dt) * (i * dt);
     traj.points.push_back(point);
   }
   while (ros::ok())
